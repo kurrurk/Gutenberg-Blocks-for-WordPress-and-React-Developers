0a6cef7f694ed26051bb9e1d363970a77b7a9c9b
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-09 15:25:24 +0100

    007. Adding a Pre-commit Git Hook with Husky

--- src/boilerplate/index.js
+++ src/boilerplate/index.js
@@ -14,8 +14,6 @@ import metadata from './block.json';
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
 
-const x = 1;
-
 registerBlockType( metadata.name, {
 	/**
 	 * @see ./edit.js
